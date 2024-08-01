7d352eb53d9c80621dafc68828655d5c0a2408da
Duda <dudacal20@gmail.com>
2024-08-01 17:57:18 -0300

    Versao 2

--- consulta.php
+++ consulta.php
@@ -1,5 +1,8 @@
 <?php
    include "conecta.php";
+
+   echo "<br><br>";
+   echo "<b>Mostrando todos os registros</b><br>";
    $sql = "SELECT id, nome, senha, email FROM usuario";
    $resultado = mysqli_query($conexao, $sql);
    echo "<br>";
@@ -12,5 +15,50 @@
    } else {
     echo "Nenhum registro encontrado.";
    }
+   echo "<br>";
+   echo "<b>Nomes que começam com 'M' </b><br>";
+   $sql = "SELECT id, nome, senha, email FROM usuario where nome like 'm%'";
+   $resultado = mysqli_query($conexao, $sql);
+   echo "<br>";
+   if (mysqli_num_rows($resultado) > 0) {
+    while($registro = mysqli_fetch_assoc($resultado)){
+        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
+        "" . $registro["senha"]. " - Email: " . $registro["email"]. 
+        "<br>";
+    }
+   } else {
+    echo "Nenhum registro encontrado.";
+   }
+   
+   echo "<br>";
+   echo "<b> Ordenar os registros por email</b><br>";
+   $sql = "SELECT id, nome, senha, email FROM usuario order by email";
+   $resultado = mysqli_query($conexao, $sql);
+   echo "<br>";
+   if (mysqli_num_rows($resultado) > 0) {
+    while($registro = mysqli_fetch_assoc($resultado)){
+        echo "id: " . $registro["id"]. " - Nome: " . $registro["nome"].
+        "" . $registro["senha"]. " - Email: " . $registro["email"]. 
+        "<br>";
+    }
+   } else {
+    echo "Nenhum registro encontrado.";
+   }
+
+
+   echo "<br>";
+   echo "<b>Verificar se existe usuário='Duda' com senha 123 e mostrar seu email.Caso não exista, mostrar uma mensagem</b><br>";
+   $sql = "SELECT id, nome, senha, email FROM usuario where nome='Duda' and senha='123'";
+   $resultado = mysqli_query($conexao, $sql);
+   echo "<br>";
+   if (mysqli_num_rows($resultado) > 0) {
+    while($registro = mysqli_fetch_assoc($resultado)){
+        echo "Email: " . $registro["email"]. "<br>";
+        
+    }
+   } else {
+    echo "Nenhum registro encontrado.";
+   }
+
 
 ?>
